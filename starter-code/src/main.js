f603e5531cb9b52d196928b3e69de2733866467d
germfy <gdominguezmontes@gmail.com>
2019-05-27 17:37:44 -0500

    cambios iniciaes

--- starter-code/src/main.js
+++ starter-code/src/main.js
@@ -1,31 +1,45 @@
-var cards = [
-  { name: 'aquaman',         img: 'aquaman.jpg' },
-  { name: 'batman',          img: 'batman.jpg' },
-  { name: 'captain america', img: 'captain-america.jpg' },
-  { name: 'fantastic four',  img: 'fantastic-four.jpg' },
-  { name: 'flash',           img: 'flash.jpg' },
-  { name: 'green arrow',     img: 'green-arrow.jpg' },
-  { name: 'green lantern',   img: 'green-lantern.jpg' },
-  { name: 'ironman',         img: 'ironman.jpg' },
-  { name: 'spiderman',       img: 'spiderman.jpg' },
-  { name: 'superman',        img: 'superman.jpg' },
-  { name: 'the avengers',    img: 'the-avengers.jpg' },
-  { name: 'thor',            img: 'thor.jpg' },
-  { name: 'aquaman',         img: 'aquaman.jpg' },
-  { name: 'batman',          img: 'batman.jpg' },
-  { name: 'captain america', img: 'captain-america.jpg' },
-  { name: 'fantastic four',  img: 'fantastic-four.jpg' },
-  { name: 'flash',           img: 'flash.jpg' },
-  { name: 'green arrow',     img: 'green-arrow.jpg' },
-  { name: 'green lantern',   img: 'green-lantern.jpg' },
-  { name: 'ironman',         img: 'ironman.jpg' },
-  { name: 'spiderman',       img: 'spiderman.jpg' },
-  { name: 'superman',        img: 'superman.jpg' },
-  { name: 'the avengers',    img: 'the-avengers.jpg' },
-  { name: 'thor',            img: 'thor.jpg' }
-];
-var memoryGame = new MemoryGame(cards);
 
+let vue = new Vue({
+  el: "#game",
+  data: {
+    cards :[
+      { name: 'aquaman', img: 'aquaman.jpg' },
+      { name: 'batman', img: 'batman.jpg' },
+      { name: 'captain america', img: 'captain-america.jpg' },
+      { name: 'fantastic four', img: 'fantastic-four.jpg' },
+      { name: 'flash', img: 'flash.jpg' },
+      { name: 'green arrow', img: 'green-arrow.jpg' },
+      { name: 'green lantern', img: 'green-lantern.jpg' },
+      { name: 'ironman', img: 'ironman.jpg' },
+      { name: 'spiderman', img: 'spiderman.jpg' },
+      { name: 'superman', img: 'superman.jpg' },
+      { name: 'the avengers', img: 'the-avengers.jpg' },
+      { name: 'thor', img: 'thor.jpg' },
+      { name: 'aquaman', img: 'aquaman.jpg' },
+      { name: 'batman', img: 'batman.jpg' },
+      { name: 'captain america', img: 'captain-america.jpg' },
+      { name: 'fantastic four', img: 'fantastic-four.jpg' },
+      { name: 'flash', img: 'flash.jpg' },
+      { name: 'green arrow', img: 'green-arrow.jpg' },
+      { name: 'green lantern', img: 'green-lantern.jpg' },
+      { name: 'ironman', img: 'ironman.jpg' },
+      { name: 'spiderman', img: 'spiderman.jpg' },
+      { name: 'superman', img: 'superman.jpg' },
+      { name: 'the avengers', img: 'the-avengers.jpg' },
+      { name: 'thor', img: 'thor.jpg' }
+    ],
+    memoryGame: new MemoryGame(cards)
+  }
+})
+
+vue.$once('DOMContentLoaded', funtion(event){
+  var html = '';
+  memoryGame.cards.forEach(function (pic) {
+    html += '<div class="card" data-card-name="' + pic.name + '">';
+    html += '  <div class="back" name="' + pic.img + '"></div>';
+    html += '  <div class="front" style="background: url(img/' + pic.img + ') no-repeat"></div>';
+    html += '</div>';
+  })
 
 document.addEventListener("DOMContentLoaded", function(event) { 
   var html = '';
--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,8 +1,33 @@
 class MemoryGame {
-  constructor(card){
+  constructor(cards) {
     this.cards = cards;
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
+    console.log(cards);
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+  shuffleCards() {
+    let longitud = this.cards.length;
+    //if (i === 0) return false;
+    while (--longitud) {
+      let j = Math.floor(Math.random() * (longitud + 1));
+      let tempi = this.cards[longitud];
+      let tempj = this.cards[j];
+      this.cards[longitud] = tempj;
+      this.cards[j] = tempi;
+    }
+  }
+  checkIfPair(card1, card2) {
+    this.pairsClicked++;
+    if (card1 === card2) {
+      this.pairsGuessed++;
+      return true;
+    }
+    if (card1 !== card2) return false;
+  }
+  isFinished() {
+    console.log(this.pairsGuessed, this.cards.length / 2, this.cards);
+    if (this.pairsGuessed < this.cards.length / 2) return false;
+    if (this.pairsGuessed == this.cards.length / 2) return true;
+  }
 }
